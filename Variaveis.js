2deaccb59ce1081b9a0ef0f250e82edb5334841f
Vini <134401761+vini-destruidor19@users.noreply.github.com>
2024-08-14 10:34:10 -0300

    Add files via upload

---
+++ Variaveis.js
@@ -0,0 +1,43 @@
+// variavel
+// tipo Number
+
+const meuNumero = 78
+const numeroUm = 19
+const numeroDois = 98
+
+const soma = numeroDois + numeroUm
+console.log(soma)
+
+const subtracao = numeroUm - numeroDois
+console.log(subtracao)
+
+const Multiplica = numeroDois * meuNumero
+console.log(Multiplica)
+
+const divisao = meuNumero / numeroDois
+console.log(divisao)
+
+// float (numero decimal)
+
+const numeroTreis = 3.37;
+const numeroQuarto = .5 //mesma coisa que 0.5
+
+const operacaoM = numeroTreis * numeroQuarto
+console.log(operacaoM)
+
+// NaN NOT nunber 
+const operacaoN ="a" * numeroTreis
+console.log(operacaoN)
+const novaOperacao = "a" + meuNumero
+console.log(novaOperacao)
+
+//string(texto)
+const meuNome = "vini"
+const frase = "meu nome é"
+const issoNaoEUmNumero = "100000"
+
+const minhaFrase = frase + meuNome
+console.log(minhaFrase)
+
+const operacao0 = 
+
