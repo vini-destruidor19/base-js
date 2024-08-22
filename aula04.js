1078434b22f4cdc602a5f810c1076639f3e8fe1b
Vini <134401761+vini-destruidor19@users.noreply.github.com>
2024-08-22 12:12:03 -0300

    Add files via upload

--- aula04.js
+++ aula04.js
@@ -15,11 +15,12 @@ function imprimeTexto (Texto){
 
 // 2) chamar/executar função
 imprimeTexto(X)
-imprimeTexto(vini)
- function soma (){
+imprimeTexto("vini")
+
+function soma (){
     return 2 + 2
  }
-console.log(sama())
+console.log(soma())
 imprimeTexto(somaNumeros(2, 3))
 imprimeTexto(somaNumeros(23, 34))
 function somaNumeros(numero1, numero2){
@@ -39,3 +40,25 @@ function Multiplicacao(numero4 = 2, numero5 = 5){
     return numero4 * numero5
 }
 imprimeTexto(Multiplicacao(soma(3,4), soma(5,10)))
+
+//contante de fuçao
+const constanteSoma = function (nume1, nume2){return nume1 + nume2}
+console.log(constanteSoma(3,6))
+
+//função e var sao "listadas" no topo do arquivo 
+
+//arrow function
+const apresentaArrow = nome =>`meu nome é ${nome}`;
+const somaEmFlechar = (nu1, nu2) => nu1 + nu2
+console.log(apresentaArrow("vini"))
+console.log(somaEmFlechar(12,98))
+
+const somaNumeroPequenos = (num1, num2)=>{
+    if (num1 > 10 || num2 > 10){
+        return "essa função e para Pequenos"
+    }
+else{
+    return num1 +num2
+    }
+}
+console.log(somaNumeroPequenos(2,1))
